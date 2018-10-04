ea7267d46b5133bd10b186ad7c7f91083f1cea92
Kevin Burke <kev@inburke.com>
2018-10-04 11:18:37 -0700

    Listen for end events
    In the event that the read stream terminates before all of the data
    has been read, the pipe will send an 'end' event. We should listen for
    this event and hit the callback with it, instead of hanging forever.
    
    Added tests to verify this. Verified the patch fixes the problem by
    running the tests without the patch, observing that they timeout, then
    running the tests with the patch and verifying you get an error back.

--- src/readBuffer.js
+++ src/readBuffer.js
@@ -3,10 +3,14 @@ export default function readBuffer(pipe, length, callback) {
     callback(null, new Buffer(0));
     return;
   }
+  let terminated = false;
   let remainingLength = length;
   const buffers = [];
   const readChunk = () => {
     const onChunk = (arg) => {
+      if (terminated) {
+        return;
+      }
       let chunk = arg;
       let overflow;
       if (chunk.length > remainingLength) {
@@ -23,10 +27,21 @@ export default function readBuffer(pipe, length, callback) {
         if (overflow) {
           pipe.unshift(overflow);
         }
+        terminated = true;
         callback(null, Buffer.concat(buffers, length));
       }
     };
+    const onEnd = () => {
+      if (terminated) {
+        return;
+      }
+      terminated = true;
+      const err = new Error(`Stream ended ${remainingLength.toString()} bytes prematurely`);
+      err.name = 'EarlyEOFError';
+      callback(err);
+    };
     pipe.on('data', onChunk);
+    pipe.on('end', onEnd);
     pipe.resume();
   };
   readChunk();
