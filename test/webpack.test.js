eb488702ca12ee11fb792bd177250293e78518b6
Benjamin Hodgson <bhodgson@stackoverflow.com>
2021-12-08 11:27:24 -0500

    fix: getLogger not defined in child loader

--- test/webpack.test.js
+++ test/webpack.test.js
@@ -2,6 +2,7 @@ import webpack from 'webpack';
 
 import sassLoaderConfig from './sass-loader-example/webpack.config';
 import tsLoaderConfig from './ts-loader-example/webpack.config';
+import lessLoaderConfig from './less-loader-example/webpack.config';
 
 test("Processes sass-loader's @import correctly", (done) => {
   const config = sassLoaderConfig({});
@@ -27,6 +28,24 @@ test('Processes ts-loader correctly', (done) => {
   const config = tsLoaderConfig({});
 
   webpack(config, (err, stats) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+
+    expect(err).toBe(null);
+
+    if (stats.hasErrors()) {
+      // eslint-disable-next-line no-console
+      console.error(stats.toJson().errors);
+    }
+    expect(stats.hasErrors()).toBe(false);
+    done();
+  });
+}, 30000);
+
+test('Works with less-loader', (done) => {
+  webpack(lessLoaderConfig, (err, stats) => {
     if (err) {
       // eslint-disable-next-line no-console
       console.error(err);
