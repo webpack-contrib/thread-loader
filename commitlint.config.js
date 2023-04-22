bf54b00672fbf67bcccf27bf0a0ddcdfa230c072
Sachin Kumar <ersachin3112@gmail.com>
2023-04-22 15:12:01 +0530

    chore: disable commit message length check

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    "header-max-length": [0],
-    "body-max-line-length": [0],
-    "footer-max-line-length": [0],
+    'header-max-length': [0],
+    'body-max-line-length': [0],
+    'footer-max-line-length': [0],
   },
 };
