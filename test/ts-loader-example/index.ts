9b263653f95730e7d647018a4adbb4d06761958a
alexander.akait <sheo13666q@gmail.com>
2023-05-22 19:42:09 +0300

    fix: compatibility with ts-loader

---
+++ test/ts-loader-example/index.ts
@@ -0,0 +1,3 @@
+const foo: string = 'foo';
+
+console.log(foo);
--- test/webpack.test.js
+++ test/webpack.test.js
@@ -1,9 +1,30 @@
 import webpack from 'webpack';
 
 import sassLoaderConfig from './sass-loader-example/webpack.config';
+import tsLoaderConfig from './ts-loader-example/webpack.config';
 
 test("Processes sass-loader's @import correctly", (done) => {
   const config = sassLoaderConfig({});
+
+  webpack(config, (err, stats) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+
+    expect(err).toBe(null);
+
+    if (stats.hasErrors()) {
+      // eslint-disable-next-line no-console
+      console.error(stats.toJson().errors);
+    }
+    expect(stats.hasErrors()).toBe(false);
+    done();
+  });
+}, 30000);
+
+test('Processes ts-loader correctly', (done) => {
+  const config = tsLoaderConfig({});
 
   webpack(config, (err, stats) => {
     if (err) {
